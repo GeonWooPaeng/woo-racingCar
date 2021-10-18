a4e4524ecbfae20f3c7f7d988dc1a34e2950309f
GeonWooPaeng <gwpaeng@naver.com>
2021-10-18 21:25:38 +0900

    feat: Make base js function

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const getRandomNum = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+const getCarTry = () => {
+  const $carTryInput = document.querySelector("#car-try-input");
+};
+
+export default function RacingCar() {
+  const $carNameSubmit = document.querySelector("#car-name-submit");
+  const $carTrySubmit = document.querySelector("car-try-submit");
+
+  $carNameSubmit.addEventListener("click", getCarName);
+  // $carTrySubmit.addEventListener("click", getCarTry);
+}
+
+new RacingCar();
