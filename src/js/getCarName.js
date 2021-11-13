2747dfa979899bd773f52e38fb5bd15ee85c0278
GeonWooPaeng <gwpaeng@naver.com>
2021-11-13 15:34:08 +0900

    Add setCarName, actionPlayerHTML

--- src/js/getCarName.js
+++ src/js/getCarName.js
@@ -8,6 +8,28 @@ const carNameCheck = (carNameList) => {
   }
   return true;
 };
+i;
+const actionPlayerHTML = (carName) => {
+  const $playerAction = document.querySelector("#player-action");
+
+  $playerAction.insertAdjacentHTML(
+    "beforeend",
+    `<div class="mr-2">
+      <div class="car-player">${carName}</div>
+      <div class="d-flex justify-center mt-3">
+        <div class="relative spinner-container">
+          <span class="material spnner"></span>
+        </div>
+      </div>
+    </div>`
+  );
+};
+
+const setCarName = (carNameList) => {
+  carNameList.forEach((carName) => {
+    actionPlayerHTML(carName);
+  });
+};
 
 export const getCarName = () => {
   const $carNameInput = document.querySelector("#car-name-input");
@@ -17,4 +39,5 @@ export const getCarName = () => {
     $carNameInput.value = "";
     alert("자동차 이름 입력이 잘못되었습니다.");
   }
+  return setCarName(carNameList);
 };
